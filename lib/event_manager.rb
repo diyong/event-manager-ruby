67d94c2e234d01e6e742ad39a271b14cf1086ee4
diyong <diyong@sandiego.edu>
2018-02-13 17:16:22 -0800

    Refractor letter generation in save_thank_you_letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,16 @@ def legislators_by_zipcode(zip)
 	end
 end 
 
+def save_thank_you_letters(id, form_letter)
+	Dir.mkdir("../output") unless Dir.exists? "../output"
+
+	filename = "../output/thanks_#{id}.html"
+
+	File.open(filename, "w") do |file|
+		file.puts form_letter
+	end
+end
+
 puts "EventManager Initialized!\n\n"
 
 contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
@@ -38,13 +48,7 @@ contents.each do |row|
 
 	form_letter = erb_template.result(binding)
 
-	Dir.mkdir("../output") unless Dir.exists? "../output"
-
-	filename = "../output/thanks_#{id}.html"
-
-	File.open(filename, "w") do |file|
-		file.puts form_letter
-	end
+	save_thank_you_letters(id, form_letter)
 end
 
 
