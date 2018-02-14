d294319dfdba60cb2ab7d49f4219e07ad85b5c85
diyong <diyong@sandiego.edu>
2018-02-13 16:43:16 -0800

    Relocate csv files to root

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -27,7 +27,7 @@ end
 
 puts "EventManager Initialized!\n\n"
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
 	name = row[:first_name]
